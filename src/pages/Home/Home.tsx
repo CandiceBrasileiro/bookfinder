c75b64874a856adcdaf28e04b4257a42d5285f65
Candice Lopes <candicelopes88@gmail.com>
2024-01-25 09:39:46 -0300

    input + css

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,11 +4,12 @@ import styles from './Home.module.css';
 const Home = () => {
   return (
     <div className={styles.container}>
-    <div className={styles.container}>
+    <div className={styles.text}>
       <p><span>Find</span> your favorite books</p>
+      <hr />
     </div>
     <div className={styles.search_area}>
-      <input type="text" />
+      <input type="text" placeholder='Type author, title...' />
     </div>
     </div>
 
