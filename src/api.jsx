94e182e9a3720df2602aa10ea4678e45bad43970
Candice Lopes <candicelopes88@gmail.com>
2024-01-28 11:45:21 -0300

    correção converter

--- src/api.jsx
+++ src/api.jsx
@@ -2,8 +2,9 @@ import axios from 'axios';
 import {booksConverter} from './converter';
 
 export const getBooks = async(search) => {
-    const response = await axios.get `https://www.googleapis.com/books/v1/volumes?q=${search}&key=AIzaSyCCAAqjtJh-HCUDD0KFpSrtUm5k2_EIos0&maxResults=40`
+    const url = `https://www.googleapis.com/books/v1/volumes?q=${search}&key=AIzaSyCCAAqjtJh-HCUDD0KFpSrtUm5k2_EIos0&maxResults=40`
+    console.log('url',url)
+    const response = await axios.get (url)
     const converter = booksConverter(response.data);
-
     return converter;
 }
